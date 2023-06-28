294e38d69ce4d337b94b1001d2b29ab6a49f3535
raulpenate <50472064+raulpenate@users.noreply.github.com>
2023-06-28 10:54:04 -0600

    🎨 (ADD): adding views
    such as app.jsx

---
+++ src/views/App.jsx
@@ -0,0 +1,17 @@
+import Food from '../components/Food'
+import HeadlinesCards from '../components/HeadlinesCards'
+import Hero from '../components/Hero'
+import Navbar from '../components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HeadlinesCards />
+      <Food />
+    </>
+  )
+}
+
+export default App
